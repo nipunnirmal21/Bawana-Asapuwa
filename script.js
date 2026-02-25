0b387e40a5b7c3273b6950d51396deb0f2e151c3
Nipun Nirmal Samarathunga <141556293+nipunnirmal21@users.noreply.github.com>
2026-02-25 22:08:31 +0530

    else

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ if (hamburger) {
         if (navLinks.classList.contains('active')) {
             icon.classList.remove('fa-bars');
             icon.classList.add('fa-times');
+            } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    });
+}
