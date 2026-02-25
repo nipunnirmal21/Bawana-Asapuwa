2e8dd163f0ac6abb19673683e1b3b4ac07af640e
Nipun Nirmal Samarathunga <141556293+nipunnirmal21@users.noreply.github.com>
2026-02-25 21:44:54 +0530

    if 1

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const navbar = document.getElementById('navbar');
 
 if (navbar) {
     window.addEventListener('scroll', () => {
-        if (window.scrollY > 50) 
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+}
