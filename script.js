56ed70ac382bccf7a2c1374125c56f7b8666a4ec
Nipun Nirmal Samarathunga <141556293+nipunnirmal21@users.noreply.github.com>
2026-02-25 21:47:51 +0530

    navLinksItems

--- script.js
+++ script.js
@@ -14,3 +14,4 @@ if (navbar) {
 // Mobile Menu Toggle
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
+const navLinksItems = document.querySelectorAll('.nav-links li a');
