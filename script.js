1a3ad6f37ce1f171cc438d08d3cae9abb991a449
Nipun Nirmal Samarathunga <141556293+nipunnirmal21@users.noreply.github.com>
2026-02-27 19:56:44 +0530

    අඩු කරන්න

--- script.js
+++ script.js
@@ -63,3 +63,5 @@ function setupLoadMore(buttonId, hiddenItemClass, sectionId){
                 });
                 icon.classList.remove('fa-chevron-down');
                 icon.classList.add('fa-chevron-up');
+                this.title = "අඩු කරන්න";
+            } else {
