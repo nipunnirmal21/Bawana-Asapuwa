464a10ab2403cb92790926da65f99eb0eabb69cc
Nipun Nirmal Samarathunga <141556293+nipunnirmal21@users.noreply.github.com>
2026-03-01 23:54:32 +0530

    Initialize Load More for History

--- script.js
+++ script.js
@@ -88,3 +88,5 @@ function setupLoadMore(buttonId, hiddenItemClass, sectionId){
 }
              // Initialize Load More for Photo Gallery       
              setupLoadMore('gallery-load-more', '.gallery-hidden', 'photo-gallery');
+
+             // Initialize Load More for History
